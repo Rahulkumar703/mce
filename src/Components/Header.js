607f0b407e906f6351b9428bc141377dc4902101
Rahul Kumar <rahulkumar703@outlook.com>
2022-06-30 00:16:30 +0530

    Done Responsive Work For Navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,7 @@ function Header() {
 
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [isThemeOpen, setIsThemeOpen] = useState(false);
+    const [isActiveHamburger, setIsActiveHamburger] = useState(false);
 
     useEffect(() => {
         if (isDarkMode) {
@@ -34,51 +35,63 @@ function Header() {
                     <h1 className='heading-text'>Motihari College of Engineering</h1>
                 </Link>
             </div>
-            <nav className='nav-bar'>
-                <ul className='nav-list'>
-                    <li className='nav-items'>
-                        <Link className='nav-links' to="/">
-                            <ion-icon className="icons" name="home-outline"></ion-icon>Home
-                        </Link>
-                    </li>
-                    <li className='nav-items'>
-                        <Link className='nav-links' to="/program">
-                            <ion-icon className="icons" name="stats-chart-outline"></ion-icon>Program
-                        </Link>
-                    </li>
-                    <li className='nav-items'>
-                        <Link className='nav-links' to="/about">
-                            <ion-icon className="icons" name="information-circle-outline"></ion-icon>About
-                        </Link>
-                    </li>
-                    <li className='nav-items'>
-                        <Link className='nav-links' to="/gallery">
-                            <ion-icon className="icons" name="images-outline"></ion-icon>Gallery
-                        </Link>
-                    </li>
-                    <li className='nav-items'>
+            <div className={isActiveHamburger ? "row active" : "row"}>
+                <nav className='nav-bar'>
+                    <ul className='nav-list'>
+                        <li className='nav-items'>
+                            <Link className='nav-links' to="/">
+                                <ion-icon className="icons" name="home-outline"></ion-icon>
+                                <p>Home</p>
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link className='nav-links' to="/program">
+                                <ion-icon className="icons" name="stats-chart-outline"></ion-icon>
+                                <p>Program</p>
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link className='nav-links' to="/about">
+                                <ion-icon className="icons" name="information-circle-outline"></ion-icon>
+                                <p>About</p>
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link className='nav-links' to="/gallery">
+                                <ion-icon className="icons" name="images-outline"></ion-icon>
+                                <p>Gallery</p>
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
 
-                        <Link className='nav-links' id='register' to="/registration">
-                            <ion-icon className="icons" name="person-add-outline"></ion-icon>Registration
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className="theme-menu">
-                <div className="dark-mode-toggle" onClick={() => { setIsDarkMode(!isDarkMode) }} >
-                    {isDarkMode ? <ion-icon name="sunny-outline"></ion-icon> : <ion-icon className="icons" name="moon-outline"></ion-icon>}
+                            <Link className='nav-links' id='register' to="/registration">
+                                <ion-icon className="icons" name="person-add-outline"></ion-icon>
+                                <p>Registration</p>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="theme-menu">
+                    <div className="dark-mode-toggle" onClick={() => { setIsDarkMode(!isDarkMode) }} >
+                        {isDarkMode ? <ion-icon name="sunny-outline"></ion-icon> : <ion-icon className="icons" name="moon-outline"></ion-icon>}
+                    </div>
+                    <div className="themecolor" onClick={() => { setIsThemeOpen(!isThemeOpen) }}>
+                        <ion-icon name="color-palette-outline"></ion-icon>
+                        {isThemeOpen ? <div className="color-palate">
+                            <div className="colors" style={{ '--color': '#f4ff30' }} onClick={changeTheme}></div>
+                            <div className="colors" style={{ '--color': '#4d14c6' }} onClick={changeTheme}></div>
+                            <div className="colors" style={{ '--color': '#ff0a0a' }} onClick={changeTheme}></div>
+                            <div className="colors" style={{ '--color': '#28ff10' }} onClick={changeTheme}></div>
+                            <div className="colors" style={{ '--color': '#9913b7' }} onClick={changeTheme}></div>
+                            <div className="colors" style={{ '--color': '#ae1a2b' }} onClick={changeTheme}></div>
+                        </div> : ''}
+                    </div>
                 </div>
-                <div className="themecolor" onClick={() => { setIsThemeOpen(!isThemeOpen) }}>
-                    <ion-icon name="color-palette-outline"></ion-icon>
-                    {isThemeOpen ? <div className="color-palate">
-                        <div className="colors" style={{ '--color': '#f4ff30' }} onClick={changeTheme}></div>
-                        <div className="colors" style={{ '--color': '#4d14c6' }} onClick={changeTheme}></div>
-                        <div className="colors" style={{ '--color': '#ff0a0a' }} onClick={changeTheme}></div>
-                        <div className="colors" style={{ '--color': '#28ff10' }} onClick={changeTheme}></div>
-                        <div className="colors" style={{ '--color': '#9913b7' }} onClick={changeTheme}></div>
-                        <div className="colors" style={{ '--color': '#ae1a2b' }} onClick={changeTheme}></div>
-                    </div> : ''}
-                </div>
+            </div>
+            <div className="hamburger" onClick={() => { setIsActiveHamburger(!isActiveHamburger) }}>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
         </header>
     )
